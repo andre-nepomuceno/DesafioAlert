abf1d8fb622c9b407929d3fef879dc2908bda340
Andre Nepomuceno <andre.nepa@gmail.com>
2022-06-02 16:46:56 -0300

    Adicionados filmes de outras categorias e splashscreen

--- DesafioAlert/Model/Filme.swift
+++ DesafioAlert/Model/Filme.swift
@@ -36,28 +36,70 @@ class Filme {
             poster: "ABruxaDeBlair"
         ))
         filmes.append(Filme(
-            titulo: "O Enigma de Outro Mundo",
-            anoDeLancamento: 1982,
-            genero: "Terror",
-            poster: "ACoisa"
-        ))
-        filmes.append(Filme(
             titulo: "Alien",
             anoDeLancamento: 1979,
             genero: "Terror",
             poster: "Alien"
         ))
         filmes.append(Filme(
-            titulo: "A Noite dos Mortos-Vivos",
-            anoDeLancamento: 1968,
-            genero: "Terror",
-            poster: "ANoiteDosMortosVivos"
-        ))
-        filmes.append(Filme(
             titulo: "A Profecia",
             anoDeLancamento: 1976,
             genero: "Terror",
             poster: "AProfecia"
+        ))
+        filmes.append(Filme(
+            titulo: "A culpa é das estrelas",
+            anoDeLancamento: 2014,
+            genero: "Romance",
+            poster: "aculpaedasestrelas"
+        ))
+        filmes.append(Filme(
+            titulo: "Amor & outras drogas",
+            anoDeLancamento: 2011,
+            genero: "Romance",
+            poster: "amoreoutrasdrogas"
+        ))
+        filmes.append(Filme(
+            titulo: "Duna",
+            anoDeLancamento: 2021,
+            genero: "Ficção Ceintífica",
+            poster: "duna"
+        ))
+        filmes.append(Filme(
+            titulo: "Marley & Eu",
+            anoDeLancamento: 2008,
+            genero: "Comédia Romântica",
+            poster: "marley"
+        ))
+        filmes.append(Filme(
+            titulo: "O estranho mundo de Jack",
+            anoDeLancamento: 1993,
+            genero: "Animação",
+            poster: "oestranhomundodejack"
+        ))
+        filmes.append(Filme(
+            titulo: "Quatro vidas de um cachorro",
+            anoDeLancamento: 2017,
+            genero: "Drama",
+            poster: "quatrovidas"
+        ))
+        filmes.append(Filme(
+            titulo: "Shrek",
+            anoDeLancamento: 2001,
+            genero: "Animação",
+            poster: "shrek"
+        ))
+        filmes.append(Filme(
+            titulo: "The Batman",
+            anoDeLancamento: 2022,
+            genero: "Aventura",
+            poster: "thebatman"
+        ))
+        filmes.append(Filme(
+            titulo: "Viva! A vida é uma festa!",
+            anoDeLancamento: 2018,
+            genero: "Animação",
+            poster: "viva"
         ))
         return filmes
     }
